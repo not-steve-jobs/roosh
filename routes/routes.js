b7138ba459d0af43f95cefa3a34a1c4e2a316f52
vahe-git <hovhannisyanvahe17@gmail.com>
2021-07-13 17:00:00 +0400

    update name

--- routes/routes.js
+++ routes/routes.js
@@ -8,16 +8,20 @@ const {community, communityRegister} = require('../controller/userContent/commun
 const {blog} = require('../controller/userContent/blog')
 const {login} = require('../controller/adminContent/admin')
 const {registerContent, registerUserContent} = require('../controller/adminContent/addBlog')
+const {registerPage, registerPageContent} = require('../controller/adminContent/addPage')
 const {adminBlog} = require('../controller/adminContent/adminblog')
 const {editPage, updateBlog} = require('../controller/adminContent/editblog')
+const {editOnePage, updatePage} = require('../controller/adminContent/editOnePage')
 const {admin_profile} = require('../controller/adminContent/admin_profile')
 const {checkIsAuthenticated, forwardAuthenticated} = require('../middleware/auth');
 const { generateUrl} = require('../controller/userContent/generateUrl')
 const {fileStorageEngine} = require('../controller/adminContent/multer')
 const {aboutPage} = require('../controller/userContent/about')
+const {inner_portfolio} = require('../controller/userContent/inner_portfolio')
 const {homeCook} = require('../controller/adminContent/homeCook')
 const { changeLanguage } = require('../controller/userContent/language')
 const { ThisBlog, deleteThisBlog} = require('../controller/adminContent/ThisBlog')
+const { thisPage, deleteThisPage} = require('../controller/adminContent/thisPage')
 const language = require('../middleware/language')
 
 const upload = multer({storage: fileStorageEngine})
@@ -32,19 +36,24 @@ router.get('/admin_profile',checkIsAuthenticated, admin_profile)
 router.get('/admin_profile/adminblog',checkIsAuthenticated, adminBlog)
 router.get('/admin',forwardAuthenticated, login)
 router.get('/admin_profile/addblog',checkIsAuthenticated, registerContent)
+router.get('/admin_profile/addPage',checkIsAuthenticated, registerPage)
 router.get('/admin_profile/editblog/:id',checkIsAuthenticated, editPage)
 router.get('/admin_profile/homecook',checkIsAuthenticated, homeCook)
 router.get('/:language/blog/:id',language, generateUrl)
 router.get('/:language',language, homePage)
-
+router.get('/:language/thisPage/:id',language, inner_portfolio)
 router.get('/admin_profile/thisBlog/:id', ThisBlog)
+router.get('/admin_profile/:id', thisPage)
+router.get('/admin_profile/editOnePage/:id',checkIsAuthenticated, editOnePage)
 
 router.post('/login',
     passport.authenticate('local', { successRedirect: '/admin_profile',
         failureRedirect: '/admin' })
 );
 router.post('/admin_profile/editblog/:id',upload.single('uploadPhoto'), updateBlog)
+router.post('/admin_profile/editOnePage/:id',upload.single('uploadPhoto'), updatePage)
 router.post('/admin_profile/addblog',upload.single('uploadPhoto'), registerUserContent)
+router.post('/admin_profile/addPage',upload.single('uploadPhoto'), registerPageContent)
 router.post('/community', communityRegister)
 router.post('/language', changeLanguage);
 router.post('/logout', async (req, res, next) =>{
@@ -65,6 +74,7 @@ router.post('/logout', async (req, res, next) =>{
 
 // router.delete('/blog/:id',deleteBlog)
 router.delete('/admin_profile/thisBlog/:id', deleteThisBlog)
+router.delete('/admin_profile/:id', deleteThisPage)
 
 
 module.exports = router
